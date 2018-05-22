a520aae639254b6ec934318a3e700f61616825ca
boomel <boomel1@gmail.com>
2018-05-22 17:01:55 +0300

    make app during the tutorial

---
+++ src/components/StartScreen.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="alert alert-info">
+        <h3>Are you ready?</h3>
+        <button class="btn btn-success" @click="$emit('lets-go')">Start</button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .alert {
+        text-align: center;
+    }
+    h3, .btn {
+        margin: 30px 0;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import appMessage from './components/Message.vue'
+import appQuestion from './components/Question.vue'
+import appResult from './components/Result.vue'
+import appStartScreen from './components/StartScreen.vue'
+
+
+Vue.component('appMessage', appMessage);
+Vue.component('appQuestion', appQuestion);
+Vue.component('appResult', appResult);
+Vue.component('appStartScreen', appStartScreen);
+
 Vue.config.productionTip = false
 
 new Vue({
